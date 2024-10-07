0c4baf82e3046ba15e242ea53fba1415533d88de
tokkk <tokikake42@gmail.com>
2024-10-07 10:12:01 +0900

    fix error trap

--- core.py
+++ core.py
@@ -63,29 +63,32 @@ def handle_message(event):
         
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:  
-        line_bot_api = MessagingApi(api_client)
+    try:
+        with ApiClient(configuration) as api_client:  
+            line_bot_api = MessagingApi(api_client)
         
-        message_content = line_bot_api.get_message_content(event.message.id )
-        img_path = rf"./image/tmp_{key_name}.png"
-        with open(img_path, "wb") as f:
-            for chunk in message_content.iter_content():
-                f.write(chunk)
+            message_content = line_bot_api.get_message_content(event.message.id )
+            img_path = rf"./image/tmp_{key_name}.png"
+            with open(img_path, "wb") as f:
+                for chunk in message_content.iter_content():
+                    f.write(chunk)
         
-        destination_key = rekognition.put_to_s3_storage(img_path, key_name)
-        if destination_key == 'err': 
-            callback_text = 'S3ストレージへの画像の保存に失敗しました'
-        else:
-            callback_text = rekognition.detect_object_by_rekognition()
-            if callback_text == 'err':
-                callback_text = 'Rekognitionによるラベル検出に失敗しました'
+            destination_key = rekognition.put_to_s3_storage(img_path, key_name)
+            if destination_key == 'err': 
+                callback_text = 'S3ストレージへの画像の保存に失敗しました'
+            else:
+                callback_text = rekognition.detect_object_by_rekognition()
+                if callback_text == 'err':
+                    callback_text = 'Rekognitionによるラベル検出に失敗しました'
 
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=f"画像メッセージを受信！！\n{callback_text}")]
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text=f"画像メッセージを受信！！\n{callback_text}")]
+                )
             )
-        )
+    except Exception as e:
+        print(e, flush=True)
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=8080)
